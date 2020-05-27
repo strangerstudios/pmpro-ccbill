08d4dd6fe8ff305967d1675a0ae29c50bd50a833
Jason Coleman <jason@strangerstudios.com>
2020-05-27 15:02:15 -0400

    Some title and description changes.

--- pmpro-ccbill.php
+++ pmpro-ccbill.php
@@ -1,11 +1,13 @@
 <?php
 /*
-Plugin Name: PMPro CCBill
-Plugin URI: 
+Plugin Name: Paid Memberships Pro - CCBill Gateway
+Plugin URI:
 Description: PMPro Gateway integration for CCBill (http://www.ccbill.com)
 Version: 0.1
-Author: greathmaster
-Author URI:
+Author: Paid Memberships Pro
+Author URI: https://www.paidmembershipspro.com
+Text Domain: pmpro-ccbill
+Domain Path: /languages
 */
 
 define("PMPRO_CCBILL_DIR", dirname(__FILE__));
@@ -15,7 +17,7 @@ require_once(PMPRO_CCBILL_DIR . "/classes/class.pmprogateway_ccbill.php");
 function pmpro_wp_ajax_ccbill_webhook()
 {
 	require_once(dirname(__FILE__) . "/webhook.php");
-	exit;	
+	exit;
 }
 add_action('wp_ajax_nopriv_ccbill-webhook', 'pmpro_wp_ajax_ccbill_webhook');
 add_action('wp_ajax_ccbill-webhook', 'pmpro_wp_ajax_ccbill_webhook');
